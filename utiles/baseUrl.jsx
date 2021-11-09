fa638e682ed9b9f595077ebb7c7da534b4480537
aiahsan <70939313+aiahsan@users.noreply.github.com>
2021-11-09 10:04:23 +0500

    Update baseUrl.jsx

--- utiles/baseUrl.jsx
+++ utiles/baseUrl.jsx
@@ -1,6 +1,6 @@
 import { create } from 'apisauce';
 export const api = create({
-  baseURL: 'http://localhost:5000/api/v1/',
+  baseURL: 'https://ecomali.herokuapp.com/api/v1/',
   headers: {
     Accept: 'application/vnd.github.v3+json',
     'Content-Type': 'application/json',
